f0e7cc13c19dd3d4e416a08797876d5acf743216
Lucas <lucasrafaael@outlook.com>
2025-05-10 20:04:13 -0300

    c2 - Adicionando evento de teclado.

--- snake.py
+++ snake.py
@@ -13,8 +13,10 @@ window = pygame.display.set_mode((WINDOWS_WIDTH, WINDOWS_HEIGHT))
 snake_pos = [(STARTING_POS_X, STARTING_POS_Y)]
 snake_surface = pygame.Surface((BLOCK, BLOCK))
 snake_surface.fill((255, 255, 255))
+direction = K_LEFT
 
 while True:
+    pygame.time.Clock().tick(30)
     window.fill((0,0,0))
 
     for event in pygame.event.get():
@@ -22,7 +24,22 @@ while True:
             pygame.quit()
             quit()
 
+        elif event.type == KEYDOWN:
+            if event.key in [K_UP, K_DOWN, K_LEFT, K_RIGHT]:
+                direction = event.key
+
     for pos in snake_pos:
         window.blit(snake_surface,pos)
 
+    if direction == K_RIGHT:
+        snake_pos[0] = snake_pos[0][0] + BLOCK, snake_pos[0][1] # Movimentação para a direita.
+
+    elif direction == K_LEFT:
+        snake_pos[0] = snake_pos[0][0] - BLOCK, snake_pos[0][1] # Movimentação para a esquerda.
+
+    elif direction == K_UP:
+        snake_pos[0] = snake_pos[0][0], snake_pos[0][1] - BLOCK # Movimentação para cima.
+
+    elif direction == K_DOWN:
+        snake_pos[0] = snake_pos[0][0], snake_pos[0][1] + BLOCK # Movimentação para baixo.
     pygame.display.update()
