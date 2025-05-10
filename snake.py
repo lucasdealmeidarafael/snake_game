b346acde7ab567c74239d3f758a3a846c270f945
Lucas <lucasrafaael@outlook.com>
2025-05-10 19:31:17 -0300

    c1 - Adicionando snake na tela.

--- snake.py
+++ snake.py
@@ -12,11 +12,17 @@ window = pygame.display.set_mode((WINDOWS_WIDTH, WINDOWS_HEIGHT))
 
 snake_pos = [(STARTING_POS_X, STARTING_POS_Y)]
 snake_surface = pygame.Surface((BLOCK, BLOCK))
+snake_surface.fill((255, 255, 255))
 
 while True:
-    window.fill()
+    window.fill((0,0,0))
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             quit()
+
+    for pos in snake_pos:
+        window.blit(snake_surface,pos)
+
+    pygame.display.update()
